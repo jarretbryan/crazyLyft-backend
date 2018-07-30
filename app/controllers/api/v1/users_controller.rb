6abd4f973f9b5055a6c4d5273cb2998fc7ef7290
jarretbryan <jarret.bryan@gmail.com>
2018-07-30 17:46:36 -0400

    built user controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::UsersController < ApplicationController
+    before_action :get_user, only: [:update]
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def update
+        @user.update(note_params)
+        if @user.save
+        render json: @user, status: :accepted
+        else
+        render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+
+
+    private
+
+    def get_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(:name, :points)
+    end
+
+end
