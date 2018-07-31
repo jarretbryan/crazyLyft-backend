855778abd0c1110d7817b30cf549dcc17b3d5a39
jarretbryan <jarret.bryan@gmail.com>
2018-07-31 10:01:35 -0400

    added rails resources for scores

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_31_135128) do
+ActiveRecord::Schema.define(version: 2018_07_31_135847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "scores", force: :cascade do |t|
+    t.integer "points"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_scores_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2018_07_31_135128) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "scores", "users"
 end
